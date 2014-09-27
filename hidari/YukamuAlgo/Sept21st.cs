70b42815a4975928f5c55acfef12145cff1a6fa2
Hidari <ika5140@gmail.com>
2014-09-28 01:05:50 +0900

    演習1-4までできたのを追加します。

---
+++ hidari/YukamuAlgo/Sept21st.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YukamuAlgo
+{
+	public class Sept21st
+	{
+		static void Main(string[] args)
+		{
+		}
+
+		public static int Max4(int a, int b, int c, int d)
+		{
+			int max = a;
+
+			if (max < b)
+			{
+				max = b;
+			}
+
+			if (max < c)
+			{
+				max = c;
+			}
+
+			if (max < d)
+			{
+				max = d;
+			}
+
+			return max;
+		}
+
+		public static int Max4_2(int a, int b, int c, int d)
+		{
+			var list = new List<int>() { a, b, c, d };
+			var max = list.Aggregate((x, y) => x > y ? x : y);
+			return max;
+		}
+
+		public static int Min3(int a, int b, int c)
+		{
+			int min = a;
+
+			if (min > b)
+			{
+				min = b;
+			}
+			if (min > c)
+			{
+				min = c;
+			}
+
+			return min;
+		}
+
+		public static int Min4(int a, int b, int c, int d)
+		{
+			int min = a;
+
+			if (min > b)
+			{
+				min = b;
+			}
+
+			if (min > c)
+			{
+				min = c;
+			}
+
+			if (min > d)
+			{
+				min = d;
+			}
+
+			return min;	
+		}
+
+		public static int Med3(int a, int b, int c)
+		{
+			var max = Sept21st.Max3(a, b, c);
+			var min = Sept21st.Min3(a, b, c);
+
+			var list = new List<int>() { a, b, c};
+			list.Remove(min);
+			list.Remove(max);
+
+			var med = list.First();
+
+			return med;
+		}
+
+		public static int Max3(int a, int b, int c)
+		{
+			int max = a;
+
+			if (max < b)
+			{
+				max = b;
+			}
+
+			if (max < c)
+			{
+				max = c;
+			}
+
+			return max;
+		}
+
+	}
+}
---
+++ hidari/YukamuAlgoTest/Sept21stTest.cs
@@ -0,0 +1,78 @@
+﻿using NUnit.Framework;
+using YukamuAlgo;
+
+namespace YukamuAlgoTest
+{
+	[TestFixture(Description = "4つの引数を渡しその中で一番大きなものを返すメソッドを作成しなさい。")]
+	public class 演習1_1
+	{
+		[Test]
+		public void Max4ReturnMaxNumber()
+		{
+			Sept21st.Max4(1, 2, 3, 4).Is(4);
+			Sept21st.Max4(2, 3, 4, 1).Is(4);
+			Sept21st.Max4(3, 4, 1, 2).Is(4);
+			Sept21st.Max4(8, 8, 9, 9).Is(9);
+			Assert.AreEqual(Sept21st.Max4(4, 5, 6, 7), 7);
+		}
+
+		[Test]
+		public void Max4_2ReturnsMaxNumber()
+		{
+			Sept21st.Max4_2(1, 2, 3, 4).Is(4);
+			Sept21st.Max4_2(2, 3, 4, 1).Is(4);
+			Sept21st.Max4_2(-2, -3, -4, -1).Is(-1);
+		}
+	}
+
+	[TestFixture(Description = "3つの引数を渡しその中で一番小さなものを返すメソッドを作成しなさい。")]
+	public class 演習1_2
+	{
+		[Test]
+		public void Min3ReturnsMinNumber()
+		{
+			Sept21st.Min3(1, 2, 3).Is(1);
+			Sept21st.Min3(2, 3, 1).Is(1);
+			Sept21st.Min3(3, 2, 1).Is(1);
+		}
+	}
+
+	[TestFixture(Description = "4つの引数を渡しその中で一番小さなものを返すメソッドを作成しなさい。")]
+	public class 演習1_3
+	{
+		[Test]
+		public void Min4ReturnsMinNumber()
+		{
+			Sept21st.Min4(1, 2, 3, 4).Is(1);
+			Sept21st.Min4(2, 3, 4, 1).Is(1);
+			Sept21st.Min4(2, 2, 2, 2).Is(2);
+		}
+	}
+
+	[TestFixture]
+	public class おまけ
+	{
+		[Test]
+		public void Max3ReturnsMaxNumber()
+		{
+			Sept21st.Max3(1, 2, 3).Is(3);
+			Sept21st.Max3(2, 3, 1).Is(3);
+			Sept21st.Max3(2, 2, 3).Is(3);
+		}
+	}
+	[TestFixture(Description = "3つの引数を渡しその中の最大でも最小でもない値を返すメソッドを作成しなさい。")]
+	public class 演習1_4
+	{
+		[Test]
+		public void Med3ReturnsMediumNumber()
+		{
+			Sept21st.Med3(1, 2, 3).Is(2);
+			Sept21st.Med3(2, 3, 1).Is(2);
+			Sept21st.Med3(3, 1, 2).Is(2);
+
+			Sept21st.Med3(2, 2, 3).Is(2);
+			Sept21st.Med3(3, 2, 3).Is(3);
+			Sept21st.Med3(3, 3, 3).Is(3);
+		}
+	}
+}
